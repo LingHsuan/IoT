091a95de70e6d517c8b5626df9acf47c4b024b27
Ling-Hsuan Lin <hsuan158620@gmail.com>
2016-11-20 20:45:30 +0800

    Update and rename plants.ino to sample/plants.ino

---
+++ sample/plants.ino
@@ -0,0 +1,68 @@
+#include <math.h>
+#include <Servo.h>
+#include <DHT.h>
+
+#define soilPin A2 // soil moisture sensor
+#define dhtPin A3 // temperature and humidity sensor
+#define dhtType DHT11
+
+int soilVal = 0;
+float soil, humidity, temperature;
+
+Servo myservo;  // create servo object to control a servo
+DHT dht(dhtPin, dhtType);
+
+void setup() {
+  myservo.attach(9);
+  dht.begin();
+  Serial.begin(9600);
+}
+
+void loop() {
+  humidity = dht.readHumidity();
+  temperature = dht.readTemperature();
+  soilVal = analogRead(soilPin);
+  soil = toPercentage(soilVal);
+
+  Serial.print("Temperature: ");
+  Serial.print(temperature, 1);
+  Serial.println("*C");
+  Serial.print("Humidity: ");
+  Serial.print(humidity, 0);
+  Serial.println("%");
+  Serial.print("Moisture: ");
+  Serial.print(soil, 0);
+  Serial.print("%\t");
+  Serial.println(soilVal);
+  Serial.println();
+
+  watering(temperature, humidity, soil);
+
+  delay(5000);
+}
+
+
+// convert value to percentage
+float toPercentage(int value) {
+  float convert = (float) abs((value - 1018.18367) / (-381.633) * 100);
+  return convert;
+}
+
+int watering(float temperature, float humidity, float moisture) {
+  if (moisture < 55) {
+    moving();
+  }
+}
+
+//servo
+void moving() {
+  for (int pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (int pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+}
