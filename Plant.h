5f1fe8dd228f4c591257db5382585352ce806fa8
Ling-Hsuan Lin <hsuan158620@gmail.com>
2016-11-23 20:56:19 +0800

    Update Plant.h

--- Plant.h
+++ Plant.h
@@ -5,41 +5,42 @@
 
 class plant // soil moisture sensor
 {
-	public:
-		plant(uint8_t dhtPin);
-	    int readMoisture();
-	    float MoiToPercentage(int moiValue);
+public:
+	plant(uint8_t dhtPin);
+	int readMoisture();
+	float MoiToPercentage(int moiValue);
   
-	private:
-	    uint8_t _moiPin;
+private:
+	uint8_t _moiPin;
 };
 
 class dht // temperature and humidity sensor
 {
-	public:
-		dht(uint8_t dhtPin);
-		void init();
-		byte read_data();
-		void set();
-		float readTemperature();
-		float readHumidity();
+public:
+	dht(uint8_t dhtPin);
+	void init();
+	byte read_data();
+	void set();
+	float readTemperature();
+	float readHumidity();
 
-	private:
-		uint8_t _dhtPin;
-		byte dat[5];
+private:
+	uint8_t _dhtPin;
+	byte dat[5];
 };
 
 class fan // mini fan sensor
 {
-	public:
-		fan(uint8_t fanPin);
-		void init();
-		int fanControl(int fanspeed);
-		void fanning(float temperature);
+public:
+	fan(uint8_t fanPin);
+	void init();
+	int fanControl(int fanspeed);
+	void fanning(float temperature);
 
-	private:
-		uint8_t _fanPin;
+private:
+	uint8_t _fanPin;
 };
 
 #endif
 // END OF FILE
+
